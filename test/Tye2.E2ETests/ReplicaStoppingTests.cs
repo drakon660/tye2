3a15e4fd13328a122d0ab6718e2be667da029698
drakon660 <drakon660@gmail.com>
2026-03-07 18:14:56 +0100

    Add label-based docker cleanup in E2E test teardowns

--- test/Tye2.E2ETests/ReplicaStoppingTests.cs
+++ test/Tye2.E2ETests/ReplicaStoppingTests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -138,7 +138,11 @@ namespace Tye2.E2ETests
                         _output.WriteLine(description);
                     }
                 }
+
+                await DockerAssert.CleanupManagedResourcesAsync(_output, application.Source.DirectoryName);
             }
         }
     }
 }
+
+
